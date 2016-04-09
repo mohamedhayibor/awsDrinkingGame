d4cff94f9e9640a1b267a9e683b4cb757c831e1b
Mohamed Hayibor <mohamedhayibor@gmail.com>
2016-04-08 20:24:12 -0400

    clean up extraneous files and refactor

--- src/components/stripe.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-// use maybe later
-export default class Stripe extends React.Component {
-  render() {
-    return <h1>Hello</h1>
-  }
-}
--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from 'react'
 import DOM from 'react-dom'
 
-// import Stripe from './components/stripe.jsx' (use maybe later)
-// Let's keep it at one container for now.
 import Container from './components/container.jsx'
 
 export class App extends React.Component {
